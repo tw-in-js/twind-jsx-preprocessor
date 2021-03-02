1c3cc4864fe466ab982325e98d5fc47cf9bbf961
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-03-02 17:49:16 -0600

    bring in code from macro

--- packages/twind-jsx-preprocessor/src/index.ts
+++ packages/twind-jsx-preprocessor/src/index.ts
@@ -1,6 +1,77 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import * as babel from "@babel/core"
+import annotateAsPure from "@babel/helper-annotate-as-pure"
+import traverse from "@babel/traverse"
+import {
+	findJsxAttributeByName,
+	getJsxAttributeName,
+	getJsxAttributeValue,
+} from "./jsx-attribute"
+
+export async function preprocess(
+	code: string,
+): Promise<babel.BabelFileResult | undefined> {
+	const root = await babel.parseAsync(code)
+	if (!root) return undefined
+
+	traverse(root, {
+		Program(program) {
+			const localImportName = program.scope.generateUid("tw")
+
+			traverse(program.node, {
+				JSXOpeningElement(path) {
+					const twAttribute = findJsxAttributeByName(path.node, "tw")
+					const twAttributeValue = getJsxAttributeValue(twAttribute)
+					if (!twAttributeValue) return
+
+					const twCall = babel.types.callExpression(
+						babel.types.identifier(localImportName),
+						[twAttributeValue],
+					)
+					annotateAsPure(twCall)
+
+					const classAttribute = findJsxAttributeByName(path.node, "className")
+					const classAttributeValue = getJsxAttributeValue(classAttribute)
+
+					const newAttributeValue = classAttributeValue
+						? babel.types.templateLiteral(
+								[
+									babel.types.templateElement({ raw: "" }),
+									babel.types.templateElement({ raw: " " }),
+									babel.types.templateElement({ raw: "" }),
+								],
+								[classAttributeValue, twCall],
+						  )
+						: twCall
+
+					path.node.attributes = path.node.attributes
+						// remove existing tw and className attributes
+						.filter((node) => {
+							const name = getJsxAttributeName(node)
+							return name !== "tw" && name !== "className"
+						})
+						// add new className attribute
+						.concat(
+							babel.types.jsxAttribute(
+								babel.types.jsxIdentifier("className"),
+								babel.types.jsxExpressionContainer(newAttributeValue),
+							),
+						)
+				},
+			})
+
+			program.node.body.unshift(
+				babel.types.importDeclaration(
+					[
+						babel.types.importSpecifier(
+							babel.types.identifier(localImportName),
+							babel.types.identifier("tw"),
+						),
+					],
+					babel.types.stringLiteral("twind"),
+				),
+			)
+		},
+	})
+
+	return (await babel.transformFromAstAsync(root)) ?? undefined
+}
---
+++ packages/twind-jsx-preprocessor/src/jsx-attribute.ts
@@ -0,0 +1,44 @@
+import { types } from "@babel/core"
+
+export function findJsxAttributeByName(
+	jsxOpeningTag: types.JSXOpeningElement,
+	name: string,
+): types.JSXAttribute | undefined {
+	for (const node of jsxOpeningTag.attributes) {
+		if (types.isJSXAttribute(node) && getJsxAttributeName(node) === name) {
+			return node
+		}
+	}
+	return undefined
+}
+
+export function getJsxAttributeName(node: types.Node): string | undefined {
+	if (types.isJSXAttribute(node) && types.isJSXIdentifier(node.name)) {
+		return node.name.name
+	}
+	return undefined
+}
+
+export function getJsxAttributeValue(
+	attributeNode: types.Node | undefined,
+): types.Expression | undefined {
+	if (!types.isJSXAttribute(attributeNode)) return
+
+	// not sure when this would happen 🤷‍♂️
+	if (!attributeNode.value) return
+
+	// an expression container is like `attr={...}`,
+	// so if it's not in a container,
+	// we can just return the literal string or JSX node here
+	if (!types.isJSXExpressionContainer(attributeNode.value)) {
+		return attributeNode.value
+	}
+
+	const innerExpression = attributeNode.value.expression
+
+	// this is the case of `attr={}`,
+	// which is syntactically valid for some reason
+	if (types.isJSXEmptyExpression(innerExpression)) return undefined
+
+	return innerExpression
+}
